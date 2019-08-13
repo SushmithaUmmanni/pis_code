master: fix implementation of backpropagation

starter_bundle/ch10-neural_network_fundamentals/pyimagesearch/nn/neuralnetwork.py
@@ -39,10 +39,11 @@ class NeuralNetwork:
             # layer together, adding an extra node for the bias
             weights = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
             self.W.append(weights / np.sqrt(layers[i]))
-            # the last two layers are a special case where the input
-            # connections need a bias term but the output does not
-            weights = np.random.randn(layers[-2] + 1, layers[-1])
-            self.W.append(weights / np.sqrt(layers[-2]))
+
+        # the last two layers are a special case where the input
+        # connections need a bias term but the output does not
+        weights = np.random.randn(layers[-2] + 1, layers[-1])
+        self.W.append(weights / np.sqrt(layers[-2]))
 
     def __repr__(self):
         """Construct and return a string that represents the network architecture
@@ -98,11 +99,10 @@ class NeuralNetwork:
             # loop over each individual data point and train our network on it
             for (x, target) in zip(X, y):
                 self.fit_partial(x, target)
-                # check to see if we should display a training update
-                if epoch == 0 or (epoch + 1) % display_update == 0:
-                    loss = self.calculate_loss(X, y)
-                    print("[INFO] epoch={}, loss={:.7f}".format(
-                        epoch + 1, loss))
+            # check to see if we should display a training update
+            if epoch == 0 or (epoch + 1) % display_update == 0:
+                loss = self.calculate_loss(X, y)
+                print("[INFO] epoch={}, loss={:.7f}".format(epoch + 1, loss))
 
     def fit_partial(self, x, y):
         """Implement backpropagation algorithm.
@@ -111,17 +111,20 @@ class NeuralNetwork:
             x {float} -- An individual data point from our design matrix
             y {float} -- The corresponding class label
         """
-        # construct our list of output activations for each layer as our data point flows through the network;
-        # the first activation is a special case -- it's just the input feature vector itself
+        # construct our list of output activations for each layer as our data point
+        # flows through the network; the first activation is a special case --
+        # it's just the input feature vector itself
         activations = [np.atleast_2d(x)]
 
         # FEEDFORWARD:
         # loop over the layers in the network
         for layer in np.arange(0, len(self.W)):
-            # feed-forward the activation at the current layer by taking the dot product between the activation and
-            # the weight matrix -- this is called the "net input" to the current layer
+            # feed-forward the activation at the current layer by taking the dot product
+            # between the activation and the weight matrix -- this is called
+            # the "net input" to the current layer
             net = activations[layer].dot(self.W[layer])
-            # computing the "net output" is simply applying our nonlinear activation function to the net input
+            # computing the "net output" is simply applying our nonlinear
+            # activation function to the net input
             out = self.sigmoid(net)
             # once we have the net output, add it to our list of activations
             activations.append(out)
@@ -134,12 +137,14 @@ class NeuralNetwork:
         # entry in the deltas is simply the error of the output layer times the derivative
         # of our activation function for the output value
         deltas = [error * self.sigmoid_deriv(activations[-1])]
-        # once you understand the chain rule it becomes super easy to implement with a `for` loop -- simply loop
-        # over the layers in reverse order (ignoring the last two since we already have taken them into account)
+        # once you understand the chain rule it becomes super easy to implement
+        # with a `for` loop -- simply loop over the layers in reverse order
+        # (ignoring the last two since we already have taken them into account)
         for layer in np.arange(len(activations) - 2, 0, -1):
-            # the delta for the current layer is equal to the delta of the *previous layer* dotted with the weight
-            # matrix of the current layer, followed by multiplying the delta by the derivative of the nonlinear
-            # activation function for the activations of the current layer
+            # the delta for the current layer is equal to the delta of the *previous layer*
+            # dotted with the weight matrix of the current layer, followed by multiplying
+            # the delta by the derivative of the nonlinear  activation function for the
+            # activations of the current layer
             delta = deltas[-1].dot(self.W[layer].T)
             delta = delta * self.sigmoid_deriv(activations[layer])
             deltas.append(delta)